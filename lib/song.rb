53cd07992cdd8a28c46085a5bebfc983bfc0b9cf
derrick b <dbozich1@gmail.com>
2018-06-20 06:13:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -64,14 +64,6 @@ class Song
     DB[:conn].execute(sql)
   end
 
-
-
-  def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql)
-  end
-
-
 end
 
 require_relative "../config/environment.rb"
